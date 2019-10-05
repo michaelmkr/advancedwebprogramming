be26f8c16a8a515c7810638c6f705b34a29b7a89
MichaelMkr <it191501@fhstp.ac.at>
2019-10-05 22:22:58 +0200

    added ButtonView Component, Navigation Element and added route for ButtonView in router.js

---
+++ src/views/ButtonView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is the new Button View</h1>
+  </div>
+</template>
+
+
+<script>
+    export default {
+        name: "button"
+    }
+</script>
+
+<style scoped>
+
+</style>
