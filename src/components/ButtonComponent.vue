9bb77eefff8c20c3e81e809535203f491b99afa3
MichaelMkr <it191501@fhstp.ac.at>
2019-10-05 17:08:24 +0200

    first try including ButtonComponent.vue

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- Accent-colored raised button with ripple -->
+  <button
+    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+    Seas Hochi
+  </button>
+</template>
+
+<script>
+    export default {
+        name: "ButtonComponent",
+        props: {
+            classes: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button-component classes="mdl-button mdl-js-button mdl-button--fab mdl-button--colored"></button-component>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import ButtonComponent from "../components/ButtonComponent";
 
 export default {
   name: 'home',
   components: {
+      ButtonComponent,
     HelloWorld,
   },
 };
