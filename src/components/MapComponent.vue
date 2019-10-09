215e8460d64ba91614681ea43a8632ddb0cf1774
MichaelMkr <it191501@fhstp.ac.at>
2019-10-09 16:23:33 +0200

    included data snippet from martin

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -31,6 +31,7 @@
 <script>
     export default {
         name: "MapComponent",
+
         data() {
             return {
                 center: {lat: 48.2139035, lng: 15.6297068},
@@ -39,9 +40,50 @@
                     {position: {lat: 48.2138446, lng: 15.6298171}}
                 ],
                 places: [],
-                currentPlace: null
-            };
+                currentPlace: null,
+                position: {
+                    lat: 48.2138999,
+                    lng: 15.6297068
+                },
+                bounds: {
+                    north: 48.216888,
+                    east: 15.637702,
+                    south: 48.212105,
+                    west: 15.627389
+                },
+                sightings: [
+                    {
+                        'sighting-id': 1,
+                        timestamp: 1533569023147,
+                        'pokedex-id': 25,
+                        position: {
+                            lat: 48.213185,
+                            lng: 15.631745
+                        }
+                    },
+                    {
+                        'sighting-id': 2,
+                        timestamp: 1533569123147,
+                        'pokedex-id': 26,
+                        position: {
+                            lat: 48.212635,
+                            lng: 15.635971
+                        }
+                    },
+                ],
+                pokemon: [
+                    {
+                        name: 'Pikachu',
+                        'pokedex-id': 25
+                    },
+                    {
+                        name: 'Raichu',
+                        'pokedex-id': 26
+                    }
+                ]
+            }
         },
+
 
         mounted() {
             // this.geolocate();
