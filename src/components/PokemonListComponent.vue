e2ec1982777efdea6ee3f2bdc3f3e6db3ae2cc31
MichaelMkr <it191501@fhstp.ac.at>
2019-10-09 14:47:40 +0200

    first try to get pokemon from the server

--- src/components/PokemonListComponent.vue
+++ src/components/PokemonListComponent.vue
@@ -5,9 +5,13 @@
     <p>{{pokemon}}</p>
     <p>-----------------------------------------------------------------</p>
     <h5>Formatted</h5>
-    <div v-for="item in pokemon">
-<!--      TODO check why item.pokedex-id does not work-->
-      <p> Name: {{ item.name }} <br> Pokedex-ID: coming soon.. </p>
+    <div v-for="item in testArray">
+      <!--      TODO check why item.pokedex-id does not work-->
+      <p> Name: {{ item.name }} <br>
+      <span ref="pokeID">Pokedex-ID: coming soon..</span>
+      </p>
+      <button @click="getDetails">show details</button>
+      <!--      TODO get details and show div with v-if -->
     </div>
     <p>-----------------------------------------------------------------</p>
   </div>
@@ -20,7 +24,23 @@
         name: "PokemonListComponent",
         data() {
             return {
-                pokemon: null
+                pokemon: null,
+                details: null,
+                testArray: [
+                    {"name": "Pikachu", "pokedex-id": 25},
+                    {"name": "Raichu", "pokedex-id": 26}
+                ]
+            }
+        },
+        methods: {
+            getDetails: function getPokemonDetails() {
+                console.log("Details for Pokemon with ID 25:");
+                const pokedexID = this.$refs.pokeID;
+                console.log(pokedexID);
+                axios
+                    .get('https://virtserver.swaggerhub.com/pgmon/ws18/1.0.0/pokemon/en/25')
+                    .then(response => (this.details = response.data));
+                console.log("Details: " + this.details);
             }
         },
         mounted() {
