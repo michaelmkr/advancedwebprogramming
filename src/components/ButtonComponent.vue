bdfd59b07a2412cd8a4636fcf04a5b9459a4ed9e
MichaelMkr <it191501@fhstp.ac.at>
2019-10-14 15:27:03 +0200

    switched button component to the version of the LV

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,26 +1,65 @@
 <template>
-  <!-- TODO check for short ternary expression -->
   <button
-    :class="
-    ['mdl-button mdl-js-button mdl-button--raised mdl-button--colored',
-     round ? 'mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored' : '',
-     flat ? 'mdl-button mdl-js-button mdl-js-ripple-effect' : '']"
-    onclick="alert('Yaaay you clicked my Button! You are awesome!')">
+    class="mdl-button mdl-js-button mdl-js-ripple-effect"
+    v-bind:class="[buttonType, buttonColor]"
+    v-bind:disabled="disabled"
+    ref="button">
+    <i v-if="icon" class="material-icons">{{icon}}</i>
     <slot></slot>
   </button>
 </template>
 
 <script>
     export default {
-        name: "ButtonComponent",
+        name: 'componentButton',
         props: {
-            round: Boolean,
-            flat: Boolean
+            icon: String,
+            disabled: {
+                type: Boolean
+            },
+            color: {
+                type: String,
+                validator: value => ['primary', 'colored', 'accent'].includes(value)
+            },
+            type: {
+                type: String,
+                validator: value => ['raised', 'fab', 'mini-fab', 'icon'].includes(value)
+            }
+        },
+        computed: {
+            buttonType () {
+                let val = '';
+                if (this.type) {
+                    val = 'mdl-button--' + this.type
+                }
+                if (this.type === 'mini-fab') {
+                    val += ' mdl-button--fab'
+                }
+                return val
+            },
+            buttonColor () {
+                let val = '';
+                if (this.color) {
+                    val = 'mdl-button--' + this.color
+                }
+                return val
+            }
+        },
+        methods: {
+            upgradeButton (button) {
+                if (window.componentHandler) {
+                    window.componentHandler.upgradeElement(button)
+                } else {
+                    window.setTimeout(this.upgradeButton, 100, button)
+                }
+            }
+        },
+        mounted () {
+            this.upgradeButton(this.$refs.button)
         }
     }
-    // TODO componentHandler.upgradeElement(button);
 </script>
 
-<style scoped>
+<style>
 
 </style>
