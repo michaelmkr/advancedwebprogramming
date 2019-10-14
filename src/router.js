0f846da1a611721346a5107b50ce26bd7b4f8508
MichaelMkr <it191501@fhstp.ac.at>
2019-10-14 20:07:06 +0200

    changed router

--- src/router.js
+++ src/router.js
@@ -24,9 +24,6 @@ export default new Router({
     {
       path: '/list-example',
       name: 'list-example',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/ListExample.vue'),
     },
   ],
