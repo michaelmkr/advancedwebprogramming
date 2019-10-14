93b1c071da9781be5de1d83d0f100063595293e1
MichaelMkr <it191501@fhstp.ac.at>
2019-10-14 15:30:43 +0200

    switched out the links for retrieving pokemon details

--- src/components/PokemonListComponent.vue
+++ src/components/PokemonListComponent.vue
@@ -38,25 +38,20 @@
                 const pokedexID = this.$refs.pokeID;
                 console.log(pokedexID);
                 axios
-                    .get('https://virtserver.swaggerhub.com/pgmon/ws18/1.0.0/pokemon/en/25')
+                    // .get('https://virtserver.swaggerhub.com/pgmon/ws18/1.0.0/pokemon/en/25')
+                    .get('https://lbartner-01.media.fhstp.ac.at:4430/pokemon/en/25')
                     .then(response => (this.details = response.data));
                 console.log("Details: " + this.details);
             }
         },
         mounted() {
             axios
-                //.get('https://virtserver.swaggerhub.com/pgmon/ws18/1.0.0/pokemon/en')
+                // .get('https://virtserver.swaggerhub.com/pgmon/ws18/1.0.0/pokemon/en')
                 .get('https://lbartner-01.media.fhstp.ac.at:4430/pokemon/en')
                 .then(response => (this.pokemon = response.data));
             console.log(this.pokemon);
         }
     }
-    // NOTES
-    // to-do-done understand why axios get is not working
-    // answer from David:
-    // axios muss man so importieren-> import axios from "axios";
-    // und dann in der promise wolltest auf die pokemons mit this.data.pokemon zugreifen. da wärs nur mehr "this.pokemon". Die data funktion wird am anfang ausgefühlt und das this, was der methode injected wird, ist ein anderes als das instanzobjekt, was du schreibst. Deswegen gehen zb auch arrow functions bei methoden ned, weil da kein this injected wird
-    // und noch zuguterletzt .. die response von axios beinhaltet auch metainfos wie http status usw, deswegen greift man aufn tatsächlichen inhalt mit response.data zu
 </script>
 
 <style scoped>
