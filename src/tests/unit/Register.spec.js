16de0520f01e4e0bb0501f54194053269fd73be2
MichaelMkr <it191501@fhstp.ac.at>
2019-11-23 14:50:47 +0100

    added unit test for Register.vue

---
+++ src/tests/unit/Register.spec.js
@@ -0,0 +1,55 @@
+import { expect } from "chai";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import { Vuelidate } from "vuelidate";
+
+const localVue = createLocalVue();
+localVue.use(Vuelidate);
+
+import Register from "../../views/Register";
+
+describe("Register", () => {
+  it("no field is allowed to be empty", () => {
+    const wrapper = shallowMount(Register, { localVue });
+    wrapper.setData({
+      name: "",
+      email: "",
+      password: "",
+      password2: ""
+    });
+    expect(wrapper.vm.$v.name.required).to.equal(false);
+    expect(wrapper.vm.$v.email.required).to.equal(false);
+    expect(wrapper.vm.$v.password.required).to.equal(false);
+    expect(wrapper.vm.$v.password2.required).to.equal(false);
+  });
+  it("name must be at least 3, password at least 8 characters long", () => {
+    const wrapper = shallowMount(Register, { localVue });
+    wrapper.setData({
+      name: "12",
+      password: "1234567"
+    });
+    expect(wrapper.vm.$v.name.minLength).to.equal(false);
+    expect(wrapper.vm.$v.password.minLength).to.equal(false);
+  });
+  it("password and password2 need to match", () => {
+    const wrapper = shallowMount(Register, { localVue });
+    wrapper.setData({
+      password: "foo123456",
+      password2: "bar123456"
+    });
+    expect(wrapper.vm.$v.password2.sameAsPassword).to.equal(false);
+  });
+  it("with correct data, the validation passes successfully", () => {
+    const wrapper = shallowMount(Register, { localVue });
+    wrapper.setData({
+      name: "peter",
+      email: "peter.urban@email.com",
+      password: "foo12345",
+      password2: "foo12345"
+    });
+    expect(wrapper.vm.$v.name.$invalid).to.equal(false);
+    expect(wrapper.vm.$v.email.$invalid).to.equal(false);
+    expect(wrapper.vm.$v.password.$invalid).to.equal(false);
+    expect(wrapper.vm.$v.password2.$invalid).to.equal(false);
+    expect(wrapper.vm.$v.$invalid).to.equal(false);
+  });
+});
