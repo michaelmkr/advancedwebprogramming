fab667d54afc009185f01c9d90971ada3af00aaf
MichaelMkr <it191501@fhstp.ac.at>
2019-11-23 14:45:36 +0100

    adapted register component like snippet

---
+++ src/components/Register.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div>
+      <label>Name: </label>
+      <input v-model="name">
+      <br>
+      <span v-if="!$v.name.required">Name is required</span>
+      <span v-else-if="!$v.name.minLength">Must conatin at least 3 characters.</span>
+    </div>
+
+    <div>
+      <label>E-Mail: </label>
+      <input v-model="email">
+      <br>
+      <span v-if="!$v.email.required">Email is required.</span>
+      <span v-else-if="!$v.email.email">Not a valid email.</span>
+    </div>
+
+    <div>
+      <label>Passwort: </label>
+      <input v-model="password">
+      <br>
+      <span v-if="!$v.password.required">Password is required.</span>
+      <span v-else-if="!$v.password.minLength">Must contain at least 8 characters.</span>
+    </div>
+
+    <div>
+      <label>Passwort bestätigen: </label>
+      <input v-model="passwordConfirm">
+      <br>
+      <span v-if="!$v.passwordConfirm.required">Password is required.</span>
+      <span v-else-if="!$v.passwordConfirm.minLength">Must contain at least 8 characters.</span>
+      <span v-else-if="!$v.passwordConfirm.sameAsPassword">Must be the same as password.</span>
+    </div>
+
+    <div class="text-center">
+      <button @click="submit" class="button" type="submit">
+        Submit
+      </button>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+    import {required, email, minLength, sameAs} from "vuelidate/lib/validators";
+
+    export default {
+        name: 'register',
+        data() {
+            return {
+                name: undefined,
+                email: undefined,
+                password: undefined,
+                passwordConfirm: undefined
+            }
+        },
+        validations: {
+            name: {required, minLength: minLength(3)},
+            email: {required, email},
+            password: {required, minLength: minLength(8)},
+            passwordConfirm: {required, sameAsPassword: sameAs('password')}
+        },
+        methods: {
+            submit(event) {
+                this.$v.$touch();
+                if (this.$v.$invalid) return;
+                // this.userRegister({
+                //     name: this.name,
+                //     email: this.email,
+                //     password: this.password,
+                //     passwordConfirm: this.passwordConfirm
+                // })
+            }
+        }
+    }
+</script>
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,12 +1,14 @@
 <template>
-    <register-component></register-component>
+<!--    <register-component></register-component>-->
+  <register></register>
 </template>
 
 <script>
     import RegisterComponent from "../components/RegisterComponent";
+    import Register from "../components/Register";
     export default {
         name: "Registration",
-        components: {RegisterComponent}
+        components: {RegisterComponent, Register}
     }
 </script>
 
