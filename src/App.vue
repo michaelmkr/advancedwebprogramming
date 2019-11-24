79bb2b3304bd00e1bc3341d5a462704ed235c109
MichaelMkr <it191501@fhstp.ac.at>
2019-11-24 20:19:15 +0100

    probably fixed header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="demo-layout-transparent mdl-layout mdl-js-layout" id="app">
-    <header class="mdl-layout__header ">
-      <div class="mdl-layout__header-row">
-        <!-- Title -->
-        <span class="mdl-layout-title">Pokemon Go</span>
-        <!-- Add spacer, to align navigation to the right -->
-        <div class="mdl-layout-spacer"></div>
-        <!-- Navigation -->
+    <!-- Always shows a header, even in smaller screens. -->
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <!-- Title -->
+          <span class="mdl-layout-title">Title</span>
+          <!-- Add spacer, to align navigation to the right -->
+          <div class="mdl-layout-spacer"></div>
+          <!-- Navigation. We hide it in small screens. -->
+          <nav class="mdl-navigation mdl-layout--large-screen-only">
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+          </nav>
+        </div>
+      </header>
+      <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">Title</span>
         <nav class="mdl-navigation">
-          <router-link class="mdl-navigation__link" to="/">Home</router-link>
-          <router-link class="mdl-navigation__link" to="/map">Map</router-link>
-          <router-link class="mdl-navigation__link" to="/register">Register</router-link>
-          <router-link class="mdl-navigation__link" to="/login">Login</router-link>
+          <a class="mdl-navigation__link" href="">Link</a>
+          <a class="mdl-navigation__link" href="">Link</a>
+          <a class="mdl-navigation__link" href="">Link</a>
+          <a class="mdl-navigation__link" href="">Link</a>
         </nav>
       </div>
-    </header>
+      <main class="mdl-layout__content">
+        <div class="page-content"><!-- Your content goes here --></div>
+      </main>
+    </div>
     <main class="mdl-layout__content">
       <router-view/>
     </main>
