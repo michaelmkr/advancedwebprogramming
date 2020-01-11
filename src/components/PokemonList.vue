13c22dbd79caaff4ccb55c8769a42f3f821258ae
Patrick <it191597@fhstp.ac.at>
2020-01-11 17:50:27 +0100

    Grundgerüst für Pokemonliste erstellt

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+  <div v-for="item of sightings" :key="item['sighting-id']">
+    <div class="demo-card-square mdl-card mdl-shadow--2dp">
+      <div class="mdl-card__title mdl-card--expand">
+        <h2 class="mdl-card__title-text">Pikachu</h2>
+      </div>
+      <div class="mdl-card__actions mdl-card--border">
+        <router-link class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
+                     to="/">{items.pokedex-id}
+        </router-link>
+      </div>
+    </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "PokemonList",
+  data () {
+    return{
+    sightings: [
+          {
+            'sighting-id': 1,
+            timestamp: 1533569023147,
+            'pokedex-id': 25,
+            position: {
+              lat: 48.213185,
+              lng: 15.631745,
+            },
+          },
+          {
+            'sighting-id': 2,
+            timestamp: 1533569123147,
+            'pokedex-id': 26,
+            position: {
+              lat: 48.212635,
+              lng: 15.635971,
+            },
+          }
+    ]}
+  }
+}
+
+</script>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <new-pokemon-map-component></new-pokemon-map-component>
+    <pokemon-list></pokemon-list>
 <!--    <pokemon-map-component></pokemon-map-component>-->
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 // import PokemonMapComponent from '../components/PokemonMap.vue';
 import NewPokemonMapComponent from '../components/NewPokemonMapComponent.vue';
+import PokemonList from '../components/PokemonList';
 
 export default {
   name: 'Map',
   components: {
     NewPokemonMapComponent,
+    PokemonList,
     // PokemonMapComponent
   },
 };
