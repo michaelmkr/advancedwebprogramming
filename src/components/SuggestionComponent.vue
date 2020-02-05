7d9a031b8e6f08a6258bc5eb39969f36e56f28f9
MichaelMkr <it191501@fhstp.ac.at>
2020-02-05 21:52:35 +0100

    added suggestedItems

--- src/components/SuggestionComponent.vue
+++ src/components/SuggestionComponent.vue
@@ -2,12 +2,12 @@
   <div>
 
     <h1>Suggestion Component</h1>
-    <vue-suggestion :items="items"
-                    v-model="item"
+    <vue-suggestion :itemTemplate="itemTemplate"
+                    :items="items"
                     :setLabel="setLabel"
-                    :itemTemplate="itemTemplate"
                     @changed="inputChange"
-                    @selected="itemSelected">
+                    @selected="itemSelected"
+                    v-model="item">
     </vue-suggestion>
     <button @click="showItems">show items</button>
   </div>
@@ -16,41 +16,44 @@
 <script>
   import itemTemplate from './ItemTemplate';
   import * as http from '../APICom/index';
+
   export default {
     name: 'SuggestionComponent',
-    data () {
+    data() {
       return {
         item: {},
         items: [
-          { id: 1, name: 'Golden Retriever'},
-          { id: 2, name: 'Cat'},
-          { id: 3, name: 'Squirrel'},
+          {id: 1, name: 'Golden Retriever'},
+          {id: 2, name: 'Cat'},
+          {id: 3, name: 'Squirrel'},
         ],
+        suggestedItems: [],
         itemTemplate,
       }
     },
     mounted() {
-      //this.getPokemon()
+      this.getPokemon()
     },
     methods: {
-      showItems(){
+      showItems() {
         console.log(this.items);
       },
-      getPokemon(){
-        http.getAllPokemon({"language":"de"}).then((response) => {
+      getPokemon() {
+        http.getAllPokemon({"language": "de"}).then((response) => {
           this.items = response.data;
         });
       },
-      itemSelected (item) {
+      itemSelected(item) {
         this.item = item;
       },
-      setLabel (item) {
-        console.log("now selected")
+      setLabel(item) {
+        console.log("now selected");
         return item.name;
       },
-      inputChange (text) {
+      inputChange(text) {
         // your search method
-        this.items = this.items.filter(item => item.name.contains(text));
+        this.suggestedItems = this.items.filter(item => item.name.includes(text));
+        //this.items = this.items.filter(item => item.name.includes(text));
         // now `items` will be showed in the suggestion list
       },
     },
