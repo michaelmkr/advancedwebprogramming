64ef5bc3b4cc35ba7f811d20ee4e3d5d0faf9861
MichaelMkr <it191501@fhstp.ac.at>
2020-02-05 17:31:54 +0100

    added an input component

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="mdl-cell mdl-cell--12-col">
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"
+         :class="{'is-invalid': !!error}"
+         ref="inputWrapper"
+    >
+      <input
+        class="mdl-textfield__input"
+        :type="type"
+        :id="label"
+        :value="value"
+        v-bind="$attrs"
+        v-on="inputListeners"
+      >
+      <label class="mdl-textfield__label" :for="label">{{labelName}}</label>
+      <span class="mdl-textfield__error" v-if="error">{{error}}</span>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import { upgradeElement } from '@/tools'
+  export default {
+    name: 'InputComponent',
+    props: {
+      disabled: {
+        type: Boolean
+      },
+      type: {
+        type: String,
+        validator: value => ['text', 'email', 'password', 'passwordRepeat'].includes(value)
+      },
+      label: {
+        type: String
+      },
+      value: {
+        type: String,
+        required: Boolean
+      },
+      error: {
+        type: String
+      }
+    },
+    computed: {
+      labelName () {
+        return this.label + (this.required ? ' *' : '')
+      },
+      inputListeners () {
+        var vm = this
+        return Object.assign({},
+          this.$listeners,
+          {
+            // This ensures that the component works with v-model
+            input: function (event) {
+              vm.$emit('input', event.target.value)
+            }
+          }
+        )
+      }
+    },
+    mounted () {
+      upgradeElement(this.$refs.inputWrapper)
+    }
+  }
+</script>
+
+<style>
+
+</style>
